5f20b610245b7cb3868078d32c30f5b1647aa6dd
Lopuh <1opuhart@gmail.com>
2023-04-15 15:31:38 +0900

    #2 basic config

--- README.md
+++ README.md
@@ -5,3 +5,12 @@
 
 ### Tasks:
 - [ ] make a description of the project
+
+### Features:
+#### User: 
+- [ ] create account (login/logout/authentication)
+- [ ] cart (add/remove/buy)
+
+#### Product:
+- [ ] categories
+- [ ] reviews
--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
+
+#CONFIG
+app.config.from_object(__name__)
+app.config['UPLOAD_FOLDER'] = './static/media'
+app.config.update(dict(DATABASE=os.path.join(app.root_path, 'database.db')))
 
 if __name__ == "__main__":
     app.run(debug=True)
